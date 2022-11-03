abcfda3040cb8eea70bfca482245a8e83f08f491
jairu <jairu@LAPTOP-ODPQNUSB>
2022-11-03 18:54:24 +0100

    Cambio en codigo

--- PracticaGitAlbaPablo/src/Practica/Hola.java
+++ PracticaGitAlbaPablo/src/Practica/Hola.java
@@ -5,6 +5,8 @@ public class Hola {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Alba, hola Pablo");
+		System.out.println("para cuando la teoria");
+		System.out.println("pepe");
 	}
 
 }
